c94346d8457f0f318e449632c883c750bfff142f
Sami-Hal <sami.hallakorpi@gmail.com>
2019-10-07 14:10:43 +0300

    Finishing touches

--- myapp.js
+++ myapp.js
@@ -3,7 +3,7 @@ const bill = document.querySelector('#bill');
 const tip = document.querySelector('#tip');
 const headers = document.querySelectorAll('h4')
 let billValue = 0;
-let tipValue = 10;
+let tipValue = 0.10;
 let splitValue = 1;
 
 
@@ -13,13 +13,13 @@ function getValue(){
         billValue = parseInt(this.value);
     } else if (this.name === 'tip') {
         tipValue = parseInt(this.value) / 100;
-        headers[1].innerHTML = "Tip: " + Math.round(tipValue * 100) + "%"
+        headers[1].innerHTML = `Tip: ${Math.round(tipValue * 100)}%`
     } else if (this.name === 'split') {
         splitValue = parseInt(this.value);
         headers[2].innerHTML = `Split (number of people): ${splitValue}`
     } else {return;}
-    bill.innerHTML = "New Bill (per person): " + (billValue * (1 + tipValue)).toFixed(2)
-    tip.innerHTML = "Tip Amount (per person): " + (billValue * tipValue).toFixed(2)
+    bill.innerHTML = `New Bill (per person): ${((billValue * (1 + tipValue)) / splitValue).toFixed(2)}€`
+    tip.innerHTML = `Tip Amount (per person): ${((billValue * tipValue) / splitValue).toFixed(2)}€`
 }
 
 
